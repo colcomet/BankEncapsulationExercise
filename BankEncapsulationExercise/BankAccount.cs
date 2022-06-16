8c2725d15cf713727845e3c84be4c2f32e9ef125
Andrew Barber <drewbarber72@gmail.com>
2022-06-15 23:09:26 -0400

    initial commit

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            bool done = false;
+            do
+            {
+                done = HandleInput(account);
+            } while (!done);
+        }
+
+        private static bool HandleInput(BankAccount account)
+        {
+            Console.WriteLine();
+            Console.Write("[D]eposit, View [B]alance, or [E]xit: ");
+
+            var entry = Console.ReadKey();
+            if (entry.KeyChar == 'E' || entry.KeyChar == 'e' || entry.KeyChar == 'X' || entry.KeyChar == 'x')
+            {
+                return true;
+            }
+            else if (entry.KeyChar == 'B' || entry.KeyChar == 'b')
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Account balance is {account.GetBalance():$0.00}");
+            }
+            else if (entry.KeyChar == 'D' || entry.KeyChar == 'd')
+            {
+                Console.WriteLine();
+                double deposit = GetParsedDepositAmount();
+                account.Deposit(deposit);
+                Console.WriteLine($"{deposit:$0.00} has been deposited.");
+            }
+            return false;
+        }
+
+        private static double GetParsedDepositAmount()
+        {
+            bool gotResult;
+            double result;
+            do
+            {
+                Console.WriteLine("Enter amount to deposit");
+                gotResult = double.TryParse(Console.ReadLine(), out result);
+            } while (!gotResult);
+            return result;
+        }
+    }
+}
